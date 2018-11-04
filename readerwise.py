0e7023eda064d4e1023597570f8229f43509646b
vagrant <vagrant@vagrant.vm>
2018-11-04 19:02:44 +0000

    Adds author_ranking function, which outputs solution to Question 2.

--- readerwise.py
+++ readerwise.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Project ReaderWise
 '''
 Project ReaderWise is an internal project commissioned by Udacity News Corp [UNC] (not a real company). This
@@ -23,7 +24,7 @@ def top_articles():
     top_arts = cur.execute(query) # Execute our query
 
     # Print out formatted results of top 3 most read articles.
-    print("-- Top 3 Most Read Articles --")
+    print("-- Top 3 Most Read Articles --\n")
     # Iterate over results table and print out well-formatted results, utilizing str.format()
     for record in cur:
         title = record[0]
@@ -36,6 +37,27 @@ def top_articles():
     db.close()
 #End top_articles function
 
+def author_ranking():
+    db = psycopg2.connect(database=DBNAME) # Open connection to news database
+    cur = db.cursor() # Create cursor for querying
 
+    query = "SELECT authors.name, view_count FROM authors, author_views WHERE authors.id = author_views.author ORDER BY view_count DESC;"
+    author_ranks = cur.execute(query)
 
+    print("\n-- Author Rankings --\n")
+
+    for record in cur:
+        auth_name = record[0]
+        auth_view_count = record[1]
+        formatted_output = "{} - {} article views".format(auth_name, auth_view_count)
+        print(formatted_output)
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
+
+# Function call to answer question 1
 top_articles()
+
+# Function call to answer question 2
+author_ranking()
